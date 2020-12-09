d1cedd3d2b7ece6ad634cb70421b85552afe18e6
Jinyuan Jia <jinyuanjia02@gmail.com>
2020-12-09 16:18:46 -0500

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ os.system("nohup python3 -u training_mnist_bagging.py --k 30 --end 1000 --gpu 0 
 os.system("nohup python3 -u training_mnist_bagging.py --k 50 --end 1000 --gpu 1 --gpum 0.05 > ./release_50.log &")
 os.system("nohup python3 -u training_mnist_bagging.py --k 100 --end 1000 --gpu 2 --gpum 0.05 > ./release_100.log &")
 
-os.system("python3 compute_certified_radius.py --k 30")
-os.system("python3 compute_certified_radius.py --k 50")
-os.system("python3 compute_certified_radius.py --k 100")
+os.system("python3 compute_certified_poisoning_size.py --k 30")
+os.system("python3 compute_certified_poisoning_size.py --k 50")
+os.system("python3 compute_certified_poisoning_size.py --k 100")
 
